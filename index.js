39155a0fe48e0b619bb08dba96e2c207d104284c
Ziyaad-Labaran <Ziyaad.zalabaran@gmail.com>
2025-06-27 14:20:57 +0000

    send

--- index.js
+++ index.js
@@ -386,7 +386,7 @@ const Graphic = document.getElementById("graph-de")
 
 webDev.addEventListener("click",()=>{
     messageDis.style.display = "flex"
-    Appear.textContent = `<h1>This is web dev<h1/>`
+    Appear.textContent = "<h1>This is web dev<h1/>"
 })
 
 Graphic.addEventListener("click",()=>{
