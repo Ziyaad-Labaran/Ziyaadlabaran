5c215fff3031a0dac8bac8acd8396d54b3a50c5e
Ziyaad-Labaran <Ziyaad.zalabaran@gmail.com>
2025-06-29 12:46:04 +0000

    send

--- index.js
+++ index.js
@@ -474,11 +474,11 @@ Graphic.addEventListener("click",()=>{
     Appear.innerHTML = `
      <section class="portfolio-section">
   <div class="portfolio-card">
-    <img src="" alt="Project thumbnail" class="portfolio-image" />
+    <img src="Ziyaad estate2.jpg" alt="Ziyaad graphic design 1" class="portfolio-image" />
     <div class="portfolio-content">
       <h3>Creative Dashboard App</h3>
       <p>A modern React dashboard with sleek UI, chart animations, and responsive design.</p>
-      <a href="" target="_blank" class="btn">View Project</a>
+      <a href="Ziyaad estate2.jpg" class="btn">View Project</a>
     </div>
   </div>
   <div class="portfolio-card">
